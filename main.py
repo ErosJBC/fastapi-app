ef7b3fb414e191e05a76eb699e28848622948c07
ErosJBC <ebazanc@uni.pe>
2024-10-09 23:18:23 -0500

    🌟Adding init main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import os
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+import uvicorn
 from database.database import Base, engine
 from router.movie import routerMovie
 from router.user import routerUser
@@ -18,3 +20,7 @@ Base.metadata.create_all(bind=engine)
 @app.get('/', tags=['Inicio'])
 def read_root():
     return HTMLResponse('<h2>Hola mundo! Este es un curso de FastAPI.</h2>')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8000))
+    uvicorn.run("main:app", host='127.0.0.1', port=port)
